051f6a0d30b4ed3a5c8b593d13265d75aa5fcf2f
Documenter.jl <documenter@juliadocs.github.io>
2024-01-13 17:21:39 +0000

    build based on 2775086

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TuXartis","category":"page"},{"location":"#TuXartis","page":"Home","title":"TuXartis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TuXartis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TuXartis]","category":"page"}]
+}
