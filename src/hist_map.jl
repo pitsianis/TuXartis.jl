a0c5ac8627f36edcd8500977c6c178595777013f
Cody Qin <125537769+CodyQin@users.noreply.github.com>
2024-01-30 00:59:26 +0800

    hist_map

--- src/hist_map.jl
+++ src/hist_map.jl
@@ -1,41 +1,55 @@
 using Plots,Graphs,SGtSNEpi
+using Statistics
 include("generate_graph.jl")
 include("plot_embedding.jl")
 
 g = generate_graph("barabasi_albert",n = 1000, d = 10, k = 3, seed =1)
-deg = degree(g)[:,:]
+deg = degree(g)
 
-vertex_indices = sortperm(deg, rev=true,dims = 1)[a]
-
-a = (1:10)
 
 Y = sgtsnepi(g;d=2)
 
 
 
-function hist_map(y::Matrix, sequence::Matrix; color=:red, highlight_indices=(1:10), large2small=true)
-    vertex_indices = sortperm(vec(sequence), rev=large2small)[highlight_indices]
+function hist_map(y::Matrix, sequence::Vector; scale = "linear", color=:red, numOfbins = 10, highlight_bin = 1)
 
-    # Create scatter plot
-    point_colors = [i in vertex_indices ? color : :lightblue for i in 1:size(y, 1)]
-    point_alphas = [i in vertex_indices ? 1 : 0.1 for i in 1:size(y, 1)]
+    widthOfbins = (maximum(sequence)-minimum(sequence))/numOfbins
+
+    min = minimum(sequence)+ widthOfbins*(highlight_bin-1)
+    max = minimum(sequence)+ widthOfbins*(highlight_bin)
+
+    highlight_indices = findall(x -> x >= min && x <= max, sequence)
+
+    if scale == "linear"
+        histogram_plot = histogram(vec(sequence),bins = minimum(sequence):widthOfbins:maximum(sequence), orientation=:vertical, legend=false,
+                                    xlims=(0, maximum(sequence)),normalize=:probability)
+    elseif scale == "ylog"
+        histogram_plot = histogram(vec(sequence),bins = minimum(sequence):widthOfbins:maximum(sequence), orientation=:vertical, legend=false,
+                                    xlims=(0, maximum(sequence)),normalize=:probability,yscale=:log10)
+    elseif scale == "xlog"
+        histogram_plot = histogram(vec(sequence),bins = minimum(sequence):widthOfbins:maximum(sequence), orientation=:vertical, legend=false,
+                                    xlims=(0, maximum(sequence)),normalize=:probability,xscale=:log10)   
+    elseif scale =="loglog"
+        histogram_plot = histogram(vec(sequence),bins = minimum(sequence):widthOfbins:maximum(sequence), orientation=:vertical, legend=false,
+                                    xlims=(0, maximum(sequence)),normalize=:probability,xscale=:log10,yscale =:log10)  
+    end
+
+    annotation_text = string("Minimum sequence: ", minimum(sequence), "\n",
+    "Maximum sequence: ", maximum(sequence), "\n",
+    "Mean sequence: ", mean(sequence), "\n",
+    "Number of bins: ", numOfbins, "\n",
+    "Highlighted bin: ", highlight_bin, "\n",
+    "Number of colored points: ", length(highlight_indices))
+    annotate!(55,0.5,text(annotation_text, :blue, :left))
+
+
+    point_colors = [i in highlight_indices ? color : :lightblue for i in 1:size(y, 1)]
+    point_alphas = [i in highlight_indices ? 1 : 0.2 for i in 1:size(y, 1)]
     scatter_plot = plot_embedding(y,color = point_colors, title = "", alpha = point_alphas, size = (350,350))
 
-    # Create the first histogram plot with specified bin edges and range
-    # histogram_plot = histogram(vec(sequence),bins = 0:1:100, orientation=:vertical, legend=false,
-    #                             xlims=(0, maximum(sequence)),ylims = (0,1),normalize=:probability)
-
-    # Create the second histogram using the same bin edges and range
-    # histogram!(histogram_plot, bins = 0:1:100,sequence[highlight_indices], color=color, orientation=:vertical,
-    #                             xlims=(0,maximum(sequence)),ylims = (0,1),normalize=:probability)
-
-    # Plot the scatter plot and histogram as subplots
-    plot(scatter_plot, histogram_plot, layout=(2, 1), size=(800, 600))
+    plot(scatter_plot, histogram_plot, layout=(2, 1), size=(800, 800))
 end
 
 
-hist_map(Y, deg, highlight_indices = (1:50))
+hist_map(Y, deg, numOfbins = 10,highlight_bin = 1,scale = "xlog")
 
-point_colors = [i in highest_degree_ver ? :red : :grey for i in 1:size(Y_v, 1)]
-point_alphas = [i in highest_degree_ver ? 1 : 0.1 for i in 1:size(Y_v, 1)]
-plot_embedding(Y_v,color = point_colors, title = "", alpha = point_alphas)
